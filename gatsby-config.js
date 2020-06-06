6489c8787ca396be44794c44bb4804b1d0b7aec9
Daniel Cheah <lienald7@hotmail.com>
2020-06-06 15:04:40 +1000

    adding styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,21 @@
+const path = require('path')
+
 module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-react-redux`,
       options: {
         pathToCreateStoreModule: './src/state/createStore',
         cleanupOnClient: true,
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        src: path.join(__dirname, 'src'),
+        pages: path.join(__dirname, 'src/pages')
       }
     }
   ]
